b09396bf4aea1e9a6075d5a14923d3190198c3d8
Paola Capita <61804889+pah-10@users.noreply.github.com>
2021-12-10 09:18:24 -0300

    Add files via upload

---
+++ C#/HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("\nA execução finalizou, tecle enter para finalizar!");
+            Console.ReadLine();
+        }
+    }
+}
